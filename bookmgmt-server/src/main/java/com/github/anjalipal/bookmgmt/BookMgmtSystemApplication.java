8b887c73fc303b9193e11f6a657c34863de190e9
Anjali Pal <cse.anjali@gmail.com>
2020-09-09 15:55:58 +0530

    Intial commit

---
+++ bookmgmt-server/src/main/java/com/github/anjalipal/bookmgmt/BookMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.anjalipal.bookmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMgmtSystemApplication.class, args);
+	}
+
+}
